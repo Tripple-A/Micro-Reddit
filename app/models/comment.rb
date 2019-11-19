be9470a1d7ef21a1cace3070bd3c1ac761386724
Izaias Neto <izaiasneto4@gmail.com>
2019-11-19 18:49:35 -0300

    Made the changes require by the TSE

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :user_id, presence: true
-  validates :post_id, presence: true
   validates :comment, presence: true
   belongs_to :post
   belongs_to :user
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@
 
 class Post < ApplicationRecord
   validates :links, presence: true
-  validates :user_id, presence: true
   belongs_to :user
   has_many :comments
 end
